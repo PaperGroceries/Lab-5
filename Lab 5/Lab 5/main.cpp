c18949aed1f3ceaa2a75578e82b0a9edf34ce697
PaperGroceries <106277251+PaperGroceries@users.noreply.github.com>
2022-06-29 13:28:45 -0700

    added array and line reader

--- Lab 5/Lab 5/main.cpp
+++ Lab 5/Lab 5/main.cpp
@@ -5,10 +5,64 @@
 //  Created by Aim Kim on 2022-06-29.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream inStream;
+
+    inStream.open("answers.txt");
+
+    if (inStream.fail()){
+        cout << "Something is wrong!." << endl;
+    }
+    else    {
+         cout << "Everything is OK." << endl;
+    }
+
+    while(true){
+        cout << "Plz, enter your next question" << endl;
+        string question;
+
+        cin >> question;
+    
+        // provide an answer - todo
+        // string anAnswer;;
+        char anAnswer[100];
+
+        // if(inStream >> anAnswer)
+        //     cout << anAnswer << endl;
+        // else    {
+        //     inStream.close();
+        //     inStream.open("answers.txt");
+        //     inStream >> anAnswer;
+        //     cout << anAnswer << endl;
+        // }
+        int counter = 0;
+        // read the next char.
+        inStream.get(anAnswer[counter++]);
+        while(!inStream.eof()){
+            // I need to read the whole line
+            if(anAnswer[counter-1] == '\n'){
+                // provide an answer
+                cout << anAnswer << endl;
+                counter = 0;
+            }
+            
+            // continue reading the character
+            // read the next char.
+            inStream.get(anAnswer[counter++]);
+        }
+    }
+
+    return -77;
 }
+    
+
+    
+    
+
